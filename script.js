73f6497f3a332b53efb3bb082247e794b977765f
DMalik214 <163868959+DMalik214@users.noreply.github.com>
2024-04-05 16:56:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const navbarLinks = document.querySelector('.navbar-links');
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
 });
+const bubblesContainer = document.getElementById('bubbles-container');
+
+document.addEventListener('mousemove', (e) => {
+  const bubble = document.createElement('div');
+  bubble.classList.add('bubble');
+  bubble.style.left = `${e.clientX}px`;
+  bubble.style.top = `${e.clientY}px`;
+  bubblesContainer.appendChild(bubble);
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 2000);
+});
